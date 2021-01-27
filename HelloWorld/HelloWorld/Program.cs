db4e674037141c2c16c870d891b956f7b6cfb8bc
tiandreturner <tiandre.turner@outlook.com>
2021-01-27 18:32:57 -0500

    removed HelloWorld

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello MAD Students!");
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("Hello MAD Students!");
+
+            Console.WriteLine("Hello World!\n");
+            Console.WriteLine("\nHello MAD Students!");
+
+            Console.Write("Hello World!\n");
+            Console.WriteLine("\nHello MAD Students!");
+        }
+    }
+}
