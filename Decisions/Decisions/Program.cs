f6ba6a4e1207ab842324d25deafd6ffe435525cf
tiandreturner <14318322+tiandreturner@users.noreply.github.com>
2021-02-08 19:10:06 -0500

    Added more code to include the string interpolation

--- Decisions/Decisions/Program.cs
+++ Decisions/Decisions/Program.cs
@@ -34,14 +34,20 @@ namespace Decisions
                 commission = salesAmount * COMMISSION_RATE;
             }
 
+            // string interpolation
+            // one way
+
             //Console.WriteLine("Salesperson: {0:C}", name);
             //Console.WriteLine("Total Sales Amount: {0:C}",  salesAmount);
             //Console.WriteLine("Commission {0:C}", commission);
 
-            // another way to do the same thing as above.
+            // a different way to do the same thing as above.
             Console.WriteLine("Salesperson: {0}\nTotal Sales Amount: {1:C}\nCommission {2:C}", name, salesAmount, commission);
 
-
+            // another way to do this
+            Console.WriteLine($"Salesperson: {name}");
+            Console.WriteLine($"Sales Amount: {salesAmount:C}");
+            Console.WriteLine($"Commission: {commission:C}");
         }
     }
 }
