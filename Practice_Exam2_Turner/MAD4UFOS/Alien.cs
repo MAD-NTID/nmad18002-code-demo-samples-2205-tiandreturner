a42c06af06f2c7b5d61fb6ae56037c7945fcaf14
tiandreturner <14318322+tiandreturner@users.noreply.github.com>
2021-03-31 20:42:21 -0400

    Mock Practical Exam

---
+++ Practice_Exam2_Turner/MAD4UFOS/Alien.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MAD4UFOS
+{
+    /**
+     * MUST HAVE COMMENTS OTHERWISE YOU LOSE POINTS!
+     *
+     */
+    class Alien
+    {
+        const char DATE_BACKSLASH = '/';
+        const char DATE_DASH = '-';
+        private DateTime date;
+
+        private DateTime Date
+        {
+            get;
+            set;
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set 
+            { 
+                if(value.Length == 0)
+                {
+                    name = "Unidentifed";
+                }
+                else
+                    name = value; 
+            }
+        }
+
+        private string location;
+
+        public string Location
+        {
+            get { return location; }
+            set 
+            {
+                if (value.Length == 0)
+                {
+                    location = "N/A";
+                }
+                else
+                    location = value;
+            }
+        }
+
+        //private string date;
+        //public string Date 
+        //{ 
+        //    get
+        //    {
+        //        return date;
+        //    }
+        //    set
+        //    {
+        //        if(value.Length == 0 || !ValidDate(value))
+        //        {
+        //            date = "03/31/2021";
+        //        }
+        //        else
+        //        {
+        //            date = value;
+        //        }
+        //    }
+        //}
+
+        // default constructor
+        public Alien()
+        {
+            Name = "Unidentified";
+            Date= DateTime.Now; //Date = "03/31/2021"
+            Location = "N/A";
+
+        }
+
+        // 3-parameterized constructor
+        public Alien(string _name, DateTime _date, string _location)
+        {
+            Name = _name;
+            Date = _date;
+            Location = _location;
+        }
+
+        public static bool ValidDate(string date)
+        {
+            string[] dateInfo = date.Split(new char[] { DATE_BACKSLASH, DATE_DASH });
+            int year = 0;
+            if(!CheckMonth(dateInfo[0]) || !CheckYear(dateInfo[2], out year) || !CheckDay(dateInfo[1], year) )
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool CheckMonth(string month)
+        {
+            if (month.Length > 2)
+            {
+                return false;
+            }
+
+            if(!((int.TryParse(month, out int monthNumber)) && (monthNumber >= 1 && monthNumber <= 12)) )
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool CheckDay(string day, int year) 
+        {
+            int dayNumber = 0;
+            if (day.Length > 2)
+            {
+                return false;
+            }
+
+            if (!((int.TryParse(day, out dayNumber)) && (dayNumber >= 1 && dayNumber <= 31)))
+            {
+                return false;
+            }
+
+            if(!(year % 4 == 0 && (dayNumber >= 1 && dayNumber <= 29)))
+            {
+                return false;
+            }
+
+
+            return true;
+        }
+
+        private static bool CheckYear(string year, out int yearDigit)
+        {
+            yearDigit = 0;
+            if(year.Length != 4)
+            {
+                return false;
+            }
+
+            return int.TryParse(year, out yearDigit);
+        }
+
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\nDate: {Date}\nLocation: {Location}";
+        }
+    }
+}
---
+++ Practice_Exam2_Turner/MAD4UFOS/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+
+namespace MAD4UFOS
+{
+    /**
+     * MUST HAVE COMMENTS OTHERWISE YOU LOSE POINTS!
+     *
+     */
+    class Program
+    {
+        static Alien[] aliens = new Alien[0];
+        static void Main(string[] args)
+        {
+            int choice;
+
+          //  DisplayHeading();
+
+            while (true)
+            {
+                choice = GetMainMenuChoice();
+                //Console.Clear();
+
+
+                switch (choice)
+                {
+
+                    case 1:
+
+                        EnterASighting();
+                        Console.Write("Enter your name: ");
+                        string name = Console.ReadLine();
+                        int number = 0;
+                        HelperMethod(name, number);
+                        break;
+                    case 2:
+                        DisplayAllSightings();
+                        
+                        break;
+                    case 3:
+                        FilterBySpecies();
+                        break;
+                    case 4:
+                        Console.WriteLine("Display Record Count");
+                        Console.WriteLine($"{aliens.Length} sightings reported");
+                        //DisplayRecordCount();
+                        break;
+                    case 5:
+                        Console.WriteLine("Exiting Program");
+                        Console.Write("Do you really want to exit (Y/N): ");
+                        string yesOrNo = Console.ReadLine();
+                        if (yesOrNo == "Y" || yesOrNo == "y")
+                        {
+                            Console.WriteLine("BYE BYE");
+                            Environment.Exit(0);
+                        }
+                        //ExitProgram();
+                        break;
+                }
+                //HelperMethod("0", 0);
+                //HelperMethod(cool: 0, something: "2");
+               
+            }
+        }
+
+        public static void HelperMethod(string something, int cool)
+        {
+            Console.WriteLine($"hello {something}");
+        }
+
+        private static void FilterBySpecies()
+        {
+            int speciesCount = 0;
+            Console.WriteLine("Filter by Species");
+
+            Console.WriteLine("Enter the species name to list: ");
+            string species = Console.ReadLine();
+
+            for (int i = 0; i < aliens.Length; i++)
+            {
+                if(aliens[i].Name == species)
+                {
+                    Console.WriteLine(aliens[i]);
+                    speciesCount++;
+                }
+            }
+            Console.WriteLine($"{speciesCount} sightings reported");
+        }
+
+        private static void DisplayAllSightings()
+        {
+            Console.WriteLine("Display All Sightings");
+            int speciesCount = 0;
+           
+            for (int i = 0; i < aliens.Length; i++)
+            {
+                Console.WriteLine(aliens[i]);
+                speciesCount++;
+            }
+
+            Console.WriteLine($"{speciesCount} sightings reported");
+        }
+
+        private static void EnterASighting()
+        {
+            Console.WriteLine("Enter A Sighting");
+
+            Console.Write("Enter the species for this sighting: ");
+            string species = Console.ReadLine();
+            if (species.Length == 0)
+            {
+                Console.WriteLine("    ===> You must enter a species");
+                return;
+            }
+
+            Console.Write("Enter a date: ");
+            DateTime date;
+            string dateString = Console.ReadLine();
+            if (dateString.Length == 0 || !DateTime.TryParse(dateString, out date))
+            {
+                Console.WriteLine("    ===> You must enter a valid date in mm/dd/yyyy format");
+                return;
+            }
+
+     
+            Console.Write("Enter the location (state) where the sighting occurred: ");
+            string location = Console.ReadLine();
+            if (location.Length == 0)
+            {
+                Console.WriteLine("    ===> You must enter a location (state)");
+                return;
+
+            }
+
+            Alien alien = new Alien(species, date, location);
+
+
+            Alien alien1 = new Alien();
+            alien1.Name = "blah";
+
+            string name1 = alien1.Name;
+
+            int currentSize = aliens.Length;
+            int newSize = currentSize + 1;
+            Array.Resize(ref aliens, newSize);
+
+            //aliens[currentSize] = alien;
+            aliens[currentSize] = new Alien(species, date, location);
+        }
+
+        private static int GetMainMenuChoice()
+        {
+            int choice = 0;
+            Console.WriteLine("Main Menu");
+            Console.WriteLine("---------");
+            Console.WriteLine("   1. Enter A Sighting");
+            Console.WriteLine("   2. Display All Sightings");
+            Console.WriteLine("   3. Display Sighting filtered by species");
+            Console.WriteLine("   4. Display Record Count");
+            Console.WriteLine("   5. Exit");
+            Console.Write("Enter your selection (1-5): ");
+
+            while (!int.TryParse(Console.ReadLine(), out choice) && (choice >= 1 && choice >=5))
+            {
+                Console.WriteLine("Invalid Input...Please Try Again");
+                Console.Write("Enter your selection (1-5): ");
+            }
+
+            return choice;
+        }
+
+        private static void DisplayHeading()
+        {
+            Console.Clear();
+            Console.WriteLine("MAD4UFOS Data Collection System");
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
