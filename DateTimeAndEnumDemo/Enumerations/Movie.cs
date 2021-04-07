68ed74280bb1cc879f0d8ea736caf50113bf161c
tiandreturner <14318322+tiandreturner@users.noreply.github.com>
2021-04-07 19:38:57 -0400

    Added another demo of how enumeration works for ICE 23

---
+++ DateTimeAndEnumDemo/Enumerations/Movie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerations
+{
+    public enum MovieType
+    {
+        Action,
+        Adventure,
+        Comedy,
+        Romance,
+        Thriller
+    }
+    class Movie
+    {
+        private string genreName;
+
+        public string GenreName
+        {
+            get { return genreName; }
+        }
+
+        public Movie()
+        {
+            GenerateRandom();
+        }
+
+        private void GenerateRandom()
+        {
+            Random rand = new Random();
+            int currentRandomNumber =  rand.Next(Enum.GetNames(typeof(MovieType)).Length);
+            MovieType currentMovieType = (MovieType)currentRandomNumber;
+            genreName = currentMovieType.ToString();
+        }
+
+    }
+}
---
+++ DateTimeAndEnumDemo/Enumerations/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Enumerations
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie = new Movie();
+            Console.WriteLine(movie.GenreName);
+        }
+    }
+}
